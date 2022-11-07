3a2cf93e7309d182b9fe5f71937f3b155ecdcbd7
sunitadhikari <sunitaadhikari2001@gmail.com>
2022-11-07 13:12:05 +0545

    message

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 
 const Section = () => {
   return (
-  <>
-  hello
-  </>
+  <div>
+  
+  OUR SERVICES
+  <link rel="shortcut icon" href="image/fav-icon-svg"></link>
+  <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+<link href="https://fonts.googleapis.com/css2?family=Karma:wght@300;400;500;600;700&family=Poppins:ital,wght@0,100;0,300;0,400;0,600;0,800;0,900;1,100;1,300;1,800;1,900&family=Roboto:ital,wght@0,300;0,400;0,900;1,100;1,300;1,900&display=swap" rel="stylesheet"></link>
+  <section id="blog">
+    <div className="blog-heading">
+      {/* <span>Development-life Skills</span> */}
+      <h2>Development-life Skills</h2>
+    </div>
+  </section>
+  </div>
   )
 }
 
